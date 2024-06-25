34af633ffb5d68cdfa8ec92b962cab02ac5b564a
Method Mkoma <gershommethod02@gmail.com>
2024-06-25 21:51:01 +0300

    fixing task 1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,12 +1,12 @@
 #!/usr/bin/nodejs
 
 export default function taskBlock(trueOrFalse) {
-  const task = false;
-  const task2 = true;
+  let task = false;
+  let task2 = true;
 
   if (trueOrFalse) {
-    const task = true;
-    const task2 = false;
+    task = false;
+    task2 = true;
   }
 
   return [task, task2];
