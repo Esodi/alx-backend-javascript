f2a7171ca7abcdf7355f0ee6272704a9928f812c
Method Mkoma <gershommethod02@gmail.com>
2024-06-26 20:33:10 +0300

    task 2

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,18 @@
+#!/usr/bin/nodejs
+
+function handleResponseFromAPI(promise) {
+  return new Promise((resolve, reject) => {
+    if (promise) {
+      resolve({ status: 200, body: 'success' });
+    } else {
+      reject(new Error(''));
+    }
+  })
+    .then((response) => {
+      console.log('Got a response from the API');
+      return response;
+    })
+    .catch((err) => err);
+}
+
+module.exports = handleResponseFromAPI;
