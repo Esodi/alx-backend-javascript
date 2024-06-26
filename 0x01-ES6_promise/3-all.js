f7c9c6da84c0db932b8a7e8e3f2e9616aa24a03d
Method Mkoma <gershommethod02@gmail.com>
2024-06-26 22:05:45 +0300

    task 3

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,16 @@
+#!/usr/bin/nodejs
+
+import { uploadPhoto, createUser } from './utils.js';
+
+function handleProfileSignup() {
+	Promise.all([uploadPhoto(), createUser()])
+		.then((results) => {
+			const [photo, user] = results;
+			console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+		})
+		.catch (error => {
+			console.error('Signup system offline');
+	});
+}
+
+module.exports = handleProfileSignup;
