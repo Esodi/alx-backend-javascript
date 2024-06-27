400b3646aaa0074b6965ed01c37cf7ef2140d6a4
Method Mkoma <gershommethod02@gmail.com>
2024-06-27 14:39:40 +0300

    task 4

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,49 @@
+#!/usr/bin/nodejs
+
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(value) {
+    if (typeof value === 'number' && value >= 0) {
+      this._amount = value;
+    } else {
+      throw new TypeError('Amount must be a number');
+    }
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(value) {
+    if (value instanceof Currency) {
+      this._currency = value;
+    } else {
+      throw new TypeError('Currency must be a currency');
+    }
+  }
+
+  displayFullPrice() {
+    console.log(`${this._amount} ${this._currency.name} (${this._currency.code})`);
+  }
+
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    if (typeof conversionRate !== 'number') {
+      throw new TypeError('conversionRate must be a number');
+    } else {
+      return amount * conversionRate;
+    }
+  }
+}
