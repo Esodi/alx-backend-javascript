e7cf3e2545e683debaf327cbc3519d5561ca67b7
Method Mkoma <gershommethod02@gmail.com>
2024-06-27 16:11:00 +0300

    task 5

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,15 @@
+#!/usr/bin/nodejs
+
+export default class Building {
+  constructor(sqft) {
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  static evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
+}
