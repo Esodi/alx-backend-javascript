f3f3773a3761ae985d96efd3e07c789858e641d4
Method Mkoma <gershommethod02@gmail.com>
2024-09-18 22:26:40 +0300

    task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,7 +1,15 @@
 console.log('Welcome to Holberton School, what is your name?');
+
 process.stdin.on('data', (data) => {
-	console.log(`Your name is: ${data.toString()}`);
-	process.on('exit', () => {
-		console.log('This important software is now closing');
-	});
+    const name = data.toString().trim();  // Trim to remove any extra new lines
+    console.log(`Your name is: ${name}`);
+    
+    // To end the process after the input
+    process.stdin.end();
 });
+
+// When the process ends, display the closing message
+process.on('exit', () => {
+    console.log('This important software is now closing');
+});
+
